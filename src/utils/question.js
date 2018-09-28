a6a2690137feb1f4e20f2c77518546b1a9218ec6
Mariusz Bugajski <m.bugajski@o2.pl>
2018-09-28 12:26:52 +0200

    Added default questions

---
+++ src/utils/question.js
@@ -0,0 +1,27 @@
+export default [
+  {
+    question: "W którym roku odbył się chrzest Polski?",
+    answers: ["966", "996", "699", "669"],
+    goodAnswers: ["966"]
+  },
+  {
+    question: "W którym roku odbył się chrzest Polski?",
+    answers: ["966", "996", "699", "669"],
+    goodAnswers: [""]
+  },
+  {
+    question: "W którym roku odbył się chrzest Polski?",
+    answers: ["966", "996", "699", "669"],
+    goodAnswers: []
+  },
+  {
+    question: "W którym roku odbył się chrzest Polski?",
+    answers: ["966", "996", "699", "669"],
+    goodAnswers: ["966", "996"]
+  },
+  {
+    question: "W którym roku odbył się chrzest Polski?",
+    answers: ["966", "996", "699", "669"],
+    goodAnswers: ["699", "669"]
+  }
+];
