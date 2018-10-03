1a467b785ed73163b19c8435628ebb3064858bdf
sirmarbug <m.bugajski@o2.pl>
2018-10-03 10:36:32 +0200

    Refactored ExamResult

--- src/components/ExamResult.vue
+++ src/components/ExamResult.vue
@@ -2,46 +2,46 @@
   <div>
     <h5>Wynik</h5>
     <h6 :class="{ 'good-mark': mark >= 50, 'bad-mark': mark < 50 }">Ocena: {{ mark }}</h6>
-    <p>Poprawne: {{ goodAns }}</p>
-    <p>Błędne: {{ badAns }}</p>
+    <p>Poprawne: {{ goodAnswers }}</p>
+    <p>Błędne: {{ badAnswers }}</p>
   </div>
 </template>
 
 <script>
+  import {mapState} from 'vuex';
 
-export default {
-  props: {
-    goodAns: {
-      type: Number
+  export default {
+    props: {
+      questionCount: {
+        type: Number,
+        required: true
+      }
     },
-    badAns: {
-      type: Number
+    data() {
+      return {
+        mark: 0
+      }
     },
-    allAns: {
-      type: Number
+    computed: {
+      ...mapState('statistics', {
+        goodAnswers: state => state.goodAnswers,
+        badAnswers: state => state.badAnswers,
+      })
+    },
+    watch: {},
+    methods: {},
+    mounted() {
+      this.mark = this.goodAnswers / this.questionCount * 100;
     }
-  },
-  data () {
-    return {
-      mark: 0
-    }
-  },
-  watch: {
-  },
-  methods: {
-  },
-  mounted() {
-    this.mark = this.goodAns / this.allAns * 100;
   }
-}
 </script>
 
 <style scoped>
-.good-mark {
-  color: green;
-}
+  .good-mark {
+    color: green;
+  }
 
-.bad-mark {
-  color: red;
-}
+  .bad-mark {
+    color: red;
+  }
 </style>
